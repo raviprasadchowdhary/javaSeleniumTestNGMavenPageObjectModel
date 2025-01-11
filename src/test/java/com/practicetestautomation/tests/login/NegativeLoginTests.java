ce7c4875b01b3779d04e680b4c6528d7f6124847
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-11 20:07:49 +0530

    commit

---
+++ src/test/java/com/practicetestautomation/tests/login/NegativeLoginTests.java
@@ -0,0 +1,15 @@
+package com.practicetestautomation.tests.login;
+
+import org.testng.annotations.Test;
+
+public class NegativeLoginTests {
+    @Test
+    public void incorrectUserNameTest(){
+
+    }
+
+    @Test
+    public void incorrectPasswordTest(){
+
+    }
+}
