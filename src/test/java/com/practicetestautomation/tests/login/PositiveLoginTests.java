30e3fc59f400491804f3fa03c60f0fed5d4a3be1
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-12 18:39:57 +0530

    TestNG commit

--- src/test/java/com/practicetestautomation/tests/login/PositiveLoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/PositiveLoginTests.java
@@ -4,14 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.text.Bidi;
 
 public class PositiveLoginTests {
     @Test
     public void testLoginFunctionality(){
         //Open page
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
         //Type username student into Username field
