8dac1fa7e5f2361a7c2f53f6d72998dad65ba188
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-12 23:46:14 +0530

    refactor

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -4,10 +4,46 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTests {
+public class LoginTests {
+
+    @Test
+    public void testLoginFunctionality(){
+        //Open page
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        //Type username student into Username field
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='username']"));
+        usernameInput.sendKeys("student");
+
+        //Type password Password123 into Password field
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordInput.sendKeys("Password123");
+
+        //Push Submit button
+        WebElement submitButton = driver.findElement(By.xpath("//button[@id='submit']"));
+        submitButton.click();
+
+        //Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        String expectedCurrentPageUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualCurrentPageUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualCurrentPageUrl, expectedCurrentPageUrl);
+
+        //Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String expectedloginSuccessMessage = "Congratulations student. You successfully logged in!";
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedloginSuccessMessage));
+
+        //Verify button Log out is displayed on the new page
+        WebElement logoutButton = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logoutButton.isDisplayed());
+
+        driver.quit();
+    }
 
     @Test
     public void incorrectUserNameTest() {
@@ -27,7 +63,7 @@ public class NegativeLoginTests {
         //Verify error message is displayed
         if (isWebElementDisplayed(findWebElementByXPath(driver, "//div[@id='error']"))){
             try {
-                wait(1);
+                wait(2);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -57,7 +93,7 @@ public class NegativeLoginTests {
         //Verify error message is displayed
         if (isWebElementDisplayed(findWebElementByXPath(driver, "//div[@id='error']"))){
             try {
-                wait(1);
+                wait(2);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
